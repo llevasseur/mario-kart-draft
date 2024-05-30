f4bf740fc3547a5671c5d88d30e2716bf310babe
Leevon Levasseur <leevonlevasseur@gmail.com>
2024-05-30 15:46:54 -0700

    testing - dnd-kit draggable, droppable

--- src/components/MapGrid/MapGrid.jsx
+++ src/components/MapGrid/MapGrid.jsx
@@ -1,13 +1,24 @@
 import "./MapGrid.scss";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import Draggable from "../Draggable/Draggable";
+import Droppable from "../Droppable/Droppable";
 
 const MapGrid = ({ selectedTracks, setSelectedTracks, sendBackToList }) => {
+  const [isDropped, setIsDropped] = useState(false);
+
   const handleClickMap = (track) => {
     sendBackToList(track);
 
     setSelectedTracks((prevSelectedTracks) =>
       prevSelectedTracks.filter((t) => t.id !== track.id)
     );
+  };
+
+  const handleDragEnd = (event) => {
+    if (event.over && event.over.id === "droppable") {
+      setIsDropped(true);
+    }
   };
 
   return (
@@ -17,18 +28,24 @@ const MapGrid = ({ selectedTracks, setSelectedTracks, sendBackToList }) => {
           <p className="map-box__description">Click Maps to Add</p>
         ) : (
           selectedTracks.map((track) => (
-            <div
-              className="map-box__list-item"
-              key={track.id}
-              onClick={() => handleClickMap(track)}
-            >
-              <img
-                className="map-box__img"
-                key={track.id}
-                src={`http://localhost:5050/images/${track.img}`}
-                alt={`Image ${track.name}`}
-              />
-            </div>
+            <DndContext onDragEnd={handleDragEnd}>
+              {!isDropped ? (
+                <Draggable>
+                  <div
+                    className="map-box__list-item"
+                    key={track.id}
+                    onClick={() => handleClickMap(track)}
+                  >
+                    <img
+                      className="map-box__img"
+                      key={track.id}
+                      src={`http://localhost:5050/images/${track.img}`}
+                      alt={`Image ${track.name}`}
+                    />
+                  </div>
+                </Draggable>
+              ) : null}
+            </DndContext>
           ))
         )}
       </div>
